e81716adf34af891d64cd8646e214e8b02f87567
Niko Granö <niko@lamia.fi>
2018-07-03 16:46:13 +0300

    Preparing the start creating the extension itself.

--- main.cpp
+++ main.cpp
@@ -1,16 +1,19 @@
 #include <phpcpp.h>
 #include <cstdlib>
 #include <iostream>
-
-Php::Value myFunction()
-{
-    return random();
-}
+#include "etype.cpp"
 
 extern "C" {
 PHPCPP_EXPORT void *get_module() {
     static Php::Extension extension("etype", "1.0");
-    extension.add<myFunction>("myFunction");
+
+    // List adds here. `extension.add<myFunction>("myFunction");`
+    // class etype.
+    Php::Class<etype> etype("Etype");
+    etype.method<&etype::test> ("test");
+    extension.add(std::move(etype));
+
+    // End of add declarations.
     return extension;
 }
 }
